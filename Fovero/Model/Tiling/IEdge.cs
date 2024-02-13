9b9e2722fbacfd70efbd270efd0d2dd28bcf495f
Antony Titsas <antony.titsas@hotmail.com>
2024-02-13 23:01:41 +0100

    Minor cleanup

--- Fovero/Model/Tiling/IEdge.cs
+++ Fovero/Model/Tiling/IEdge.cs
@@ -1,13 +1,7 @@
-﻿using Fovero.Model.Geometry;
-
-namespace Fovero.Model.Tiling;
+﻿namespace Fovero.Model.Tiling;
 
 public interface IEdge
 {
-    Point2D Start { get; }
-
-    Point2D End { get; }
-
     IReadOnlyList<ITile> Neighbors { get; }
 
     bool IsShared => Neighbors.Count > 1;
