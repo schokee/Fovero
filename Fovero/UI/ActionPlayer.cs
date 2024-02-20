9ee86e33ed6ce604ae941d74d9f9c6c929bb0384
Antony Titsas <antony.titsas@hotmail.com>
2024-02-20 18:15:02 +0100

    Merged delay calculation

--- Fovero/UI/ActionPlayer.cs
+++ Fovero/UI/ActionPlayer.cs
@@ -1,12 +1,28 @@
-﻿namespace Fovero.UI;
+﻿using Caliburn.Micro;
 
-public sealed class ActionPlayer
+namespace Fovero.UI;
+
+public sealed class ActionPlayer : PropertyChangedBase
 {
+    private int _animationSpeed = 75; // 10ms delay
+
     public bool IsAnimated { get; set; } = true;
 
-    public int AnimationSpeed { get; set; } = 18;
+    public int AnimationSpeed
+    {
+        get => _animationSpeed;
+        set
+        {
+            if (Set(ref _animationSpeed, value))
+            {
+                NotifyOfPropertyChange(nameof(AnimationDelay));
+            }
+        }
+    }
 
-    public int MaximumSpeed { get; } = 20;
+    public int MaximumSpeed { get; } = 100;
+
+    public double AnimationDelay => AnimationSpeed == MaximumSpeed ? 0 : Math.Pow(10, CalculateExponent());
 
     public async Task Play<T>(IEnumerable<T> source, Action<T> doWork)
     {
@@ -23,5 +39,16 @@ public sealed class ActionPlayer
             var delay = TimeSpan.FromMilliseconds((MaximumSpeed - AnimationSpeed) * 10);
             await Task.Delay(delay);
         }
+    }
+
+    private double CalculateExponent()
+    {
+        var x = (MaximumSpeed - AnimationSpeed) / 50f;
+
+        return x switch
+        {
+            <= 1 => 2 * x,
+            _ => x + 1
+        };
     }
 }
